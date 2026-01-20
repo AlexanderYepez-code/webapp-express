21ffe591fbff47ddab63ab2d8af1060bc14b5096
AlexanderYepez-code <alexsaomn17@gmail.com>
2026-01-20 18:03:31 +0100

    feat: lista stampata

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 import express from "express"
 import moviesRouter from "./routers/movies.js"
+import cors from "cors"
 
 const app = express()
 
 const port = process.env.SERVER_PORT;
 
+app.use(
+    cors({
+        origin:"http://localhost:5173",
+    })
+);
+
+app.use(express.json())
 app.use ("/api/movies", moviesRouter);
+app.use(express.static("public"));
+
 
 app.listen(port,() => {
     console.log("Il server sta funzionando nella porta" + port)
